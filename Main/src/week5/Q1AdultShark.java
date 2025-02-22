660952569401a114afd968e46699e413ab6987e9
zinnnn37 <zinnnn37@gmail.com>
2025-02-22 23:20:35 +0900

    [WEEK5][1] pass

--- Main/src/week5/Q1AdultShark.java
+++ Main/src/week5/Q1AdultShark.java
@@ -3,85 +3,70 @@ package week5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
-enum State {
-	UP(0),
-	DOWN(1),
-	LEFT(2),
-	RIGHT(3);
-
-	private final int value;
-
-	State(int value) {
-		this.value = value;
-	}
-
-	public int getValue() {
-		return value;
-	}
-}
-
 public class Q1AdultShark {
+	private static final int UP    = 1;
+	private static final int DOWN  = 2;
+	private static final int LEFT  = 3;
+	private static final int RIGHT = 4;
 
-	private static final int[] dx = { -1, 1, 0, 0 };
-	private static final int[] dy = { 0, 0, -1, 1 };
+	private static final int[] dx = { 0, -1, 1, 0, 0 };
+	private static final int[] dy = { 0, 0, 0, -1, 1 };
 
 	private static int N;  // size
 	private static int M;  // nums of shark
 	private static int K;  // duration
 
-	private static Shark[]   sharks; // manage shark -> map으로 바꿀까,, key = id
-	private static Block[][] matrix;
-	private static int[][][] priorities;
-	
+	private static HashMap<Integer, Shark> sharks;
+	private static Block[][]               matrix;
+	private static int[][][]               priorities;
+
 	private static class Shark {
-		int   id;
-		int   x;
-		int   y;
-		State dir;
+		int x;
+		int y;
+		int dir;
+
+		Shark(int x, int y, int dir) {
+			this.x   = x;
+			this.y   = y;
+			this.dir = dir;
+		}
 	}
-	
+
 	private static class Block {
-		int   sentOf;	// shark id
-		int   duration;
-		Shark shark;
-		
-		Block(int sentOf, int duration, Shark shark) {
-			this.sentOf = sentOf;
+		int shark;
+		int sentOf;
+		int duration;
+
+		Block(int shark, int sentOf, int duration) {
+			this.shark    = shark;
+			this.sentOf   = sentOf;
 			this.duration = duration;
-			this.shark = shark;
 		}
-		
+
 		public void decreaseDuration() {
-			duration--;
+			if (duration > 0) {
+				duration--;
+				if (duration == 0) {
+					shark  = 0;
+					sentOf = 0;
+				}
+			}
 		}
-		
-		public void setDuration(int duration) {
-			this.duration = duration;
-		}
-		
-		public void setShark(Shark shark) {
-			this.shark = shark;
-		}
-		
-		public boolean isSmellRemain() {
-			return duration > 0;
-		}
-		
+
 		public void setNewSmell(int sharkId) {
-			this.sentOf = sharkId;
+			this.shark    = sharkId;
+			this.sentOf   = sharkId;
 			this.duration = K;
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		sol();
-	}
-
-	private static void sol() throws IOException {
 		init();
-		simul();
+
+		System.out.println(solve());
 	}
 
 	private static void init() throws IOException {
@@ -92,37 +77,128 @@ public class Q1AdultShark {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		// TODO 상어 아이디 0부터!!!!!!!!
+		matrix     = new Block[N + 1][N + 1];
+		sharks     = new HashMap<>();
+		priorities = new int[M + 1][5][4];
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				matrix[i][j] = new Block(0, 0, 0);
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				int sharkNum = Integer.parseInt(st.nextToken());
+				if (sharkNum > 0) {
+					sharks.put(sharkNum, new Shark(i, j, 0));
+					matrix[i][j] = new Block(sharkNum, sharkNum, K);
+				}
+			}
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= M; i++) {
+			if (sharks.containsKey(i)) {
+				sharks.get(i).dir = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 1; i <= M; i++) {
+			for (int j = 1; j <= 4; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < 4; k++) {
+					priorities[i][j][k] = Integer.parseInt(st.nextToken());
+				}
+			}
+		}
 	}
 
-	private static void simul() {
-		int time = -1;
+	private static int solve() {
+		int time = 0;
 
-		while (++time <= 1000) {
-			if (isOneSharkLeft()) {
-				System.out.println(time);
-				return;
+		while (time <= 1000) {
+			if (sharks.size() == 1 && sharks.containsKey(1)) {
+				return time;
 			}
 
-			moveShark();
-			decreaseDurationOfSmell();
+			for (int i = 1; i <= M; i++) {
+				if (sharks.containsKey(i)) {
+					Shark cur = sharks.get(i);
+					matrix[cur.x][cur.y].shark = 0;
+				}
+			}
+
+			for (int i = 1; i <= M; i++) {
+				if (!sharks.containsKey(i)) continue;
+				moveShark(i);
+			}
+
+			decreaseSmell();
+
+			for (int i = 1; i <= M; i++) {
+				if (sharks.containsKey(i)) {
+					Shark cur = sharks.get(i);
+					matrix[cur.x][cur.y].setNewSmell(i);
+				}
+			}
+			time++;
 		}
-		System.out.println(-1);
+
+		return -1;
 	}
 
-	private static boolean isOneSharkLeft() {
-		// logic,,
-		return true;
-	}
+	private static void moveShark(int sharkNum) {
+		Shark cur = sharks.get(sharkNum);
+		int   nx  = -1;
+		int   ny  = -1;
+		int   nd  = -1;
 
-	private static void moveShark() {
-		for (int i = 0; i < M; i++) {
+		int[] pos = findNextPosition(sharkNum, cur, true);
 
+		if (pos == null) {
+			pos = findNextPosition(sharkNum, cur, false);
+		}
+
+		if (pos != null) {
+			nx = pos[0];
+			ny = pos[1];
+			nd = pos[2];
+
+			if (matrix[nx][ny].shark > 0) {
+				sharks.remove(sharkNum);
+			} else {
+				matrix[nx][ny].shark = sharkNum;
+
+				cur.x   = nx;
+				cur.y   = ny;
+				cur.dir = nd;
+			}
 		}
 	}
 
-	private static void decreaseDurationOfSmell() {
-		// TODO 상어 없는데 냄새는 있는 곳 decreaseDuration()
+	private static void decreaseSmell() {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				matrix[i][j].decreaseDuration();
+			}
+		}
 	}
 
+	private static int[] findNextPosition(int sharkNum, Shark cur, boolean findEmpty) {
+		for (int i = 0; i < 4; i++) {
+			int dir = priorities[sharkNum][cur.dir][i];
+			int tx  = cur.x + dx[dir];
+			int ty  = cur.y + dy[dir];
+
+			if (tx < 1 || tx > N || ty < 1 || ty > N) continue;
+
+			if ((findEmpty && matrix[tx][ty].sentOf == 0) ||
+			    (!findEmpty && matrix[tx][ty].sentOf == sharkNum)) {
+				return new int[] { tx, ty, dir };
+			}
+		}
+		return null;
+	}
 }
