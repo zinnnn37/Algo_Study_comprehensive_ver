aafa57bd3ee06f59b2b0e86217231e4aeed3948d
zinnnn37 <zinnnn37@gmail.com>
2025-02-27 17:55:55 +0900

    [WEEK6][1] doing

---
+++ Main/src/week6/Q1WizardSharkAndFireBall.java
@@ -0,0 +1,17 @@
+package week6;
+
+public class Q1WizardSharkAndFireBall {
+	
+	// ↑, ↗, →, ↘, ↓, ↙, ←, ↖
+	private static final int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	private static final int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
+	
+	private static int N;
+	private static int M;
+	private static int K;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
