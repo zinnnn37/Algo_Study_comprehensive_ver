edf1a9e128f400246a546e035c4fe8fea6147e7c
zinnnn37 <zinnnn37@gmail.com>
2025-03-06 17:52:38 +0900

    [WEEK7][2] draft

---
+++ Main/src/week7/Q2RotatingDiscs.java
@@ -0,0 +1,46 @@
+package week7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2RotatingDiscs {
+	
+	private static BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+	
+	private static int N; // discs
+	private static int M; // nums
+	private static int T; // cnt
+	
+	private static int[] idx; // north
+	
+	private static int[][] discs;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void init() throws IOException {
+		
+	}
+	
+	private static void sol() {
+		
+	}
+	
+	private static boolean checkAdjacent() {
+		boolean flag = false;
+		
+		// TODO: check adjacent numbers && rm same nums
+		// hate flag🤮
+		
+		return flag;
+	}
+	
+	private static void setNewValue() {
+		// TODO: get avg && if big -1, if small +1
+	}
+
+}
